403598b7409a0a069e40b97fac589cafe35d3175
Michael Hotwagner <mhotwagner@gmail.com>
2018-11-14 18:46:17 -0500

    some shit

--- 4dig.ino
+++ 4dig.ino
@@ -1,22 +1,24 @@
+#include <SimpleDHT.h>
+
+#define DHT_PIN D5
 #define DATA_PIN D6
 #define CLOCK_PIN D7
 #define LATCH_PIN D8
 
+// sensor info
+SimpleDHT11 dhtSensor(DHT_PIN);
+int dhtFrequency = 1000;
+int dhti = 0;
+byte temperature = 1;
+byte _humidity; // humidity throwaway
+
+
+// display info
 int digitPins[] = { D1, D2, D3, D4 };
 int digits[] = { 252, 96, 218, 242, 102, 182, 190, 224, 254, 246 };
 int valueDigits[4];
 int blank = 0;
 int error[] = { 10, 10, 158, blank };
-
-
-void setup() {
-  pinMode(DATA_PIN, OUTPUT);
-  pinMode(CLOCK_PIN, OUTPUT);
-  pinMode(LATCH_PIN, OUTPUT);
-  for (int i = 0; i < 10; i++) {
-    pinMode(digitPins[i], OUTPUT);
-  }
-}
 
 void resetAll() {
   for (int j = 0; j < 4; j++) digitalWrite(digitPins[j], HIGH);
@@ -39,7 +41,7 @@ void setValueDigits(int value) {
   digitValue = value / 1000;
   // bail if the number's too big
   if (digitValue > 9) {
-    memcpy(valueDigits, error, 4);
+    memcpy(valueDigits, error, sizeof(valueDigits));
     return;
   }
   valueDigits[3] = digits[digitValue];
@@ -57,7 +59,7 @@ void setValueDigits(int value) {
   valueDigits[0] = digits[digitValue];
 
   int i = 3;
-  while (valueDigits[i] == digits[0]) {
+  while (valueDigits[i] == digits[0] && i > 1) {
     valueDigits[i] = blank;
     i--;
   }
@@ -70,10 +72,27 @@ void writeToDisplay(int value) {
   writeDigit(digitPins[2], valueDigits[1]);
   writeDigit(digitPins[3], valueDigits[0]);
 }
-void updateDisplay() {
-  writeToDisplay(98012);
+void updateTemperature() {
+  Serial.println('Getting temperature...');
+  dhtSensor.read(&temperature, &_humidity, NULL);
+  Serial.println(temperature);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(DATA_PIN, OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+  pinMode(LATCH_PIN, OUTPUT);
+  for (int i = 0; i < 10; i++) {
+    pinMode(digitPins[i], OUTPUT);
+  }
 }
 
 void loop() {
-  updateDisplay();
+  if (dhti == dhtFrequency) {
+    dhti = 0;
+    updateTemperature();
+  }
+  dhti++;
+  writeToDisplay(temperature);
 }
